13fd6012ac73730c467d5f32cb7d32b0f75e907f
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-06 16:42:29 -0500

    create contact form

--- reyes-doula/src/components/Contact.js
+++ reyes-doula/src/components/Contact.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('gmail', 'TEMPLATE_NAME_HERE', e.target, 'USER_NAME_HERE')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+
     return (
-        <div>
+        <div >
             <p>Welcome to the Contact Form page!</p>
+            <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div>
+                        <label for="name">Your Name</label>
+                        <input type="text" name="name" id="name" required/>
+                        <div class="validate"></div>
+                    </div>
+                    <div>
+                        <label for="email">Your Email</label>
+                        <input type="email" name="email" id="email" data-msg="Please enter a valid email" required/>
+                        <div class="validate"></div>
+                    </div>
+                    </div>
+                    <div>
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" rows="10" data-rule="required" data-msg="Please write something for Rosalie Reyes" required></textarea>
+                        <div class="validate"></div>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit">Send Message</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 };
