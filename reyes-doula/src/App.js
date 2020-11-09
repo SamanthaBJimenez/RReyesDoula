e73a4dc34ceb2ab31d798c2f79d23d685563d84c
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-09 06:47:21 -0500

    implement react-router-dom in app.js and index.js

--- reyes-doula/src/App.js
+++ reyes-doula/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import Landing from './components/Landing';
@@ -7,9 +8,17 @@ import Landing from './components/Landing';
 function App() {
   return (
     <div className="App">
-      <Landing/>
-      <Contact/>
-      <Error/>
+      <Switch>
+        <Route exact path={"/"}>
+          <Landing/>
+        </Route>
+        <Route exact path={"/contact"}>
+          <Contact/>
+        </Route>
+        <Route path={"*"}>
+          <Error/>
+        </Route>
+      </Switch>
     </div>
   );
 }
