9581686854430d03cfa7abb80bc43eb89f7695cb
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-11 06:11:52 -0500

    create bio.js

---
+++ reyes-doula/src/components/Bio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Bio = () => {
+    return (
+        <Container>
+            <p>Information about Rosalie Reyes</p>
+        </Container>
+    )
+};
+
+export default Bio;
