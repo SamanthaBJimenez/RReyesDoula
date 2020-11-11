8d27e5fce7ab9b220e00218bab8020a01450c87b
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-11 06:15:02 -0500

    create education.js page

---
+++ reyes-doula/src/components/Education.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Education = () => {
+    return (
+        <Container>
+            <p>Rosalie's Education background</p>
+        </Container>
+    )
+};
+
+export default Education;
